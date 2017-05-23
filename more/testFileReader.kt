b28f9fd8613ddeb861615f94e1333cefcbcd7a00
hchiam <hchiam@gmail.com>
2017-05-23 15:21:34 -0400

    format more like original

--- more/testFileReader.kt
+++ more/testFileReader.kt
@@ -10,26 +10,25 @@ import java.io.*
 import java.util.StringTokenizer
 
 object testFileReader {
-    @Throws(Exception::class)
-    @JvmStatic fun main(args: Array<String>) {
-        val numOfTokens = 3
-        try {
-            val file = FileReader("testfile.txt")
-            val reader = BufferedReader(file)
-            var line: String? = reader.readLine()
-            while (line != null) {
-                val tokens = StringTokenizer(line)
-                if (tokens.countTokens() == numOfTokens) {
-                    for (i in 1..numOfTokens) {
-                        println(tokens.nextToken())
-                    }
-                }
-                line = reader.readLine()
-            }
-        } catch (e: Exception) {
-            System.err.println("Exception")
-            // do something
+  @Throws(Exception::class)
+  @JvmStatic fun main(args: Array<String>) {
+    val numOfTokens = 3
+    try {
+      val file = FileReader("testfile.txt")
+      val reader = BufferedReader(file)
+      var line: String? = reader.readLine()
+      while (line != null) {
+        val tokens = StringTokenizer(line)
+        if (tokens.countTokens() == numOfTokens) {
+          for (i in 1..numOfTokens) {
+            println(tokens.nextToken())
+          }
         }
-
+        line = reader.readLine()
+      }
+    } catch (e: Exception) {
+      System.err.println("Exception")
+      // do something
     }
+  }
 }
